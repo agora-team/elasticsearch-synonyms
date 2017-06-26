3b9dac2bec526243aea39cafc9c083d326e8e99c
Prashant Sinha <prashant+git@noop.pw>
2017-06-26 18:07:21 +0530

    Update synlint parser rules

--- es_synonyms/parser.py
+++ es_synonyms/parser.py
@@ -1,16 +1,31 @@
-from .exceptions import ParserError, ParserWarning
+from .exceptions import ParserError, ParserWarning, InvalidSynonyms
 from .constants import OPERATORS, LEVELS
-from .report import ReportMessage
+from .report import ReportMessage, validate_report
 
 def empty_line(line):
+  '''Empty lines are ignored by Solr.'''
   if len(line.strip()) == 0:
     return True
 
 def comment_line(line):
-  if line.startswith('#'):
+  '''Comments start with a pound.'''
+  if line.startswith(OPERATORS['comment']):
     return True
 
 def mapping_line(line):
+  '''A mapping should contain single `=>` and may contain several `,`.'''
+  token_empty = lambda x: len(x.strip()) == 0
+  all_tokens = []
+
+  if OPERATORS['map'] in line:
+    for l in line.split(OPERATORS['map']):
+      all_tokens += l.split(OPERATORS['delimiter'])
+  else:
+    all_tokens = line.split(OPERATORS['delimiter'])
+
+  if any(map(token_empty, all_tokens)):
+    raise ParserError('Invalid mapping. Check for spaces, and trailing commas.')
+
   if OPERATORS['map'] in line:
     if not line.count(OPERATORS['map']) == 1:
       raise ParserError('Cannot define multiple mapping in single line')
@@ -23,30 +38,36 @@ def mapping_line(line):
     if not len(tokens_l) == len(tokens_r) and not len(tokens_r) == 1:
       raise ParserWarning('Unbalanced tokens in mapping')
 
-    return True
+  return True
 
 
 class SynParser(object):
+  '''Parse Solr Synonyms'''
   hooks = [empty_line, comment_line, mapping_line]
 
-  def parse(self, content=None, filename=None):
+  @classmethod
+  def parse(cls, content=None, filename=None):
     if content is not None:
-      return self.parse_lines(content.splitlines())
+      return cls.parse_lines(content.splitlines())
     elif filename is not None:
       with open(filename, 'r') as fp:
-        return self.parse_lines(fp)
+        return cls.parse_lines(fp)
     else:
       raise TypeError('Either content or filename is required')
 
-  def parse_lines(self, lines):
+  @classmethod
+  def parse_lines(cls, lines):
+    '''Parse a list of lines.'''
     reports = []
     for line_nb, line in enumerate(lines):
-      response = self.parse_line(line, line_nb)
+      response = cls.parse_line(line, line_nb)
       reports.append(response)
     return reports
 
-  def parse_line(self, line, line_nb):
-    for hook in self.hooks:
+  @classmethod
+  def parse_line(cls, line, line_nb):
+    '''Parse a single line.'''
+    for hook in cls.hooks:
       try:
         resp = hook(line)
         if resp is True:
@@ -57,3 +78,23 @@ class SynParser(object):
         return ReportMessage(LEVELS['error'], str(e), line_nb + 1)
 
     return ReportMessage(LEVELS['warn'], 'Undefined parser state', line_nb + 1)
+
+  @classmethod
+  def validate(cls, content):
+    report = cls.parse(content)
+    if not validate_report(report):
+      raise InvalidSynonyms('The synonyms file is invalid.')
+
+  @classmethod
+  def get_mapping(cls, content):
+    try:
+      cls.validate(content)
+    except InvalidSynonyms as e:
+      raise e
+    else:
+      mappings = []
+      for mline in content.splitlines():
+        l = mline.strip()
+        if len(l) and not l.startswith(OPERATORS['comment']):
+          mappings.append(l)
+      return mappings
--- es_synonyms/synlint.py
+++ es_synonyms/synlint.py
@@ -7,7 +7,6 @@ from .report import print_reports
 
 
 def cli():
-  parser = SynParser()
   cli = argparse.ArgumentParser(description='Parse and validate Solr Synonyms file.')
   cli.add_argument('file', help='Solr Synonyms file', nargs='+')
   cli.add_argument('-v', '--verbose', action='store_true', help='Print a verbose report')
@@ -17,7 +16,7 @@ def cli():
   status = 0
   for fname in args.file:
     hues.info('Validating', fname)
-    reports = parser.parse(filename=fname)
+    reports = SynParser.parse(filename=fname)
     error = print_reports(reports, args.verbose)
     if error:
       status += 1
